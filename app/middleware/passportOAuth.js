7348421ffd8cb4db21e43e142c6a747baa50b930
DwijVirani <71424352+DwijVirani@users.noreply.github.com>
2022-07-14 16:20:53 +0530

    Updated passportOauth

--- app/middleware/passportOAuth.js
+++ app/middleware/passportOAuth.js
@@ -8,10 +8,8 @@ module.exports = (passport) => {
       {
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: [
-          'https://iscribblers-dev-backend.herokuapp.com/api/auth/google/callback',
-          'http://localhost:3000/api/auth/google/callback',
-        ],
+        callbackURL: 'https://iscribblers-dev-backend.herokuapp.com/api/auth/google/callback',
+
         passReqToCallback: true,
       },
       async function (request, accessToken, refreshToken, profile, done) {
